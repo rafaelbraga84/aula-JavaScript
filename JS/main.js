f14ecd5ac26056f93e1e3e4785dfa114c00cbdcd
rafaelbraga84 <rafael_rabraga@hotmail.com>
2021-05-15 17:13:28 -0300

    aula 1 JS dio

---
+++ JS/main.js
@@ -0,0 +1,31 @@
+
+var nome = "Rafael Braga";
+var idade = 30;
+var idade2 = 6;
+
+alert("Bem vindo " + nome + ", que tem " + idade + idade2 + " anos");
+
+// comentario simples
+
+/* comentario
+com mais
+linhas
+*/
+
+// NO 2º ALERTA: somando string = concatenando:
+
+var id = "30";
+var id2 = "20";
+
+alert("somando/concatenando strings: id + id2 = " +id+id2);
+
+console.log(nome);
+console.log(idade);
+
+var frase = "AAA é melhor";
+console.log(frase.replace("AAA", "outracoisa"));
+alert(frase.replace("AAA", "B"));
+
+//console.log(frase.toUpperCase()); ou toLowerCase - CAIXA ALTA ou tudo miniscul
+
+
