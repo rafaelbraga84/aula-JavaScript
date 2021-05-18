2bf860850016e41248abf0c3b9dd1fcc75c90e69
rafaelbraga84 <rafael_rabraga@hotmail.com>
2021-05-18 00:28:25 -0300

    aula3
    prompt
    if/else
    while/for
    date

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,4 @@
-
+/*
 var nome = "Rafael Braga";
 var idade = 30;
 var idade2 = 6;
@@ -15,6 +15,7 @@ linhas
 
 // NO 2º ALERTA: somando string = concatenando:
 
+/*
 var id = "30";
 var id2 = "20";
 
@@ -25,8 +26,48 @@ console.log(idade);
 
 var frase = "AAA é melhor";
 console.log(frase.replace("AAA", "outracoisa"));
-alert(frase.replace("AAA", "B"));
+alert(frase.replace("AAA", "B")); 
+
 
 //console.log(frase.toUpperCase()); ou toLowerCase - CAIXA ALTA ou tudo miniscul
 
+*/
 
+// aula 3: condicionais, est repet. e date
+
+// comando PROMPT: caixa de pergunta
+// neste caso esta guardando a resp na var idade
+/*  IF / ELSE
+
+var idade = prompt("Qual sua idade? ");
+
+if(idade>18){
+    alert("maior de idade!");
+}else{
+    alert("menor de idade");
+}
+*/
+
+/* WHILE e FOR
+
+var count = 0;
+while(count<=5){
+    console.log(count);
+    count++;
+};
+
+var count;
+for(count=0; count<5; count++){
+    alert(count);
+}
+*/
+
+/* DATAS:
+
+var d = new Date();
+alert(d);
+alert(d.getMonth()+1);
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
